30e9bc94891e166ac17db0c8f6f46fdb53b39c12
Juan Felipe Muñoz Fernández <jfmunozf@unal.edu.co>
2016-08-10 07:08:19 -0500

    Update Animal.java

--- Taller2/src/Animal.java
+++ Taller2/src/Animal.java
@@ -1,30 +1,64 @@
-public class FamiliaAnimales {
-	public static void main(String args[]) {
-		Animal animal1 = new Animal();
-		Animal.totalAnimales++;
-		Animal animal2 = new Animal();
-		Animal.totalAnimales++;
-		
-		animal1.nombre = "Cebra";
-		animal1.genero = "F";
-		
-		animal2.nombre = "Caballo";
-		animal2.peso = new Double(98.0);
-		
-		System.out.println(animal1.nombre + " se va a casar con " + animal2.nombre);
-		animal1.setPareja(animal2);
-		animal2.setPareja(animal1);
-		
-		Familia familia = animal1.procrear("Cebrallo");
+class Animal {
+	String nombre;
+	String genero = "M";
+	double peso;
+	Animal pareja;
+	static int totalAnimales;
+	
+	void setPareja(Animal pareja) {
+		this.pareja = pareja;
+	}
 
-		if (familia != null) {
-			familia.imprimir();
-			System.out.println(familia.hijo);
-		} else {
-			System.out.println("No se pudo formar familia");
+	Animal tenerHijo (String nombre) {
+		Animal hijo = new Animal();
+		totalAnimales++;
+		hijo.nombre = nombre;
+		hijo.peso = 1;
+		double random = Math.random();
+		if (random < 0.5) 
+			hijo.genero = "F";
+		else
+			hijo.genero = "M";
+		
+		return hijo;
+	}
+	
+	Familia procrear(String nombreHijo) {
+		
+		if (pareja == null) {
+			System.out.println(this.nombre + " no tiene pareja");
+			return null;
 		}
-		System.out.println("Total de animales: " + Animal.totalAnimales);
-		Animal.morir(animal2);
-		System.out.println("Nuevo total de animales: " + Animal.totalAnimales);
+		
+		if (pareja.genero.equals(genero)) {
+			System.out.println(nombre + " y " + pareja.nombre + " son del mismo genero");
+			return null;
+		}
+		
+		System.out.println(nombre + " y " + pareja.nombre + " van a tener un hijo");
+				
+		Familia f = new Familia();
+		
+		if (this.genero.equals("M")) {
+			f.mama = pareja;
+			f.papa = this;
+		} else {
+			f.mama = this;
+			f.papa = this.pareja;
+		}
+		
+		Animal hijo = f.mama.tenerHijo(nombreHijo);
+		f.hijo = hijo;
+		System.out.println(hijo.nombre + " ha llegado al mundo");
+		return f;
+	}
+	
+	static void morir (Animal animal) {
+		System.out.println(animal.nombre + " ha muerto");
+		totalAnimales--;
+	}
+	
+	public String toString() {
+		return "Me llamo " + nombre + " y peso " + peso;
 	}
 }
